c433f895aec15cad5659b52efa3ea4d5eac7eefc
Chardy C. Fernando <ccfernando@crc.pshs.edu.ph>
2021-05-20 09:41:03 +0800

    Create sqrt.cpp

---
+++ Week37/sqrt.cpp
@@ -0,0 +1,14 @@
+//Square Root
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    double x, result;
+
+    cout<<"Enter the value of x: "<<endl;
+    cin>>x;
+    result = sqrt(x);
+    cout<<"The Square Root of "<<x<<" is "<<result<<endl;
+}
